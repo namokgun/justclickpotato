1c8e925b73bd1c95d70918fda9487ee9c37efa12
namokgun <namokgun123@gmail.com>
2026-07-19 01:31:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,33 +3,58 @@ const potato = document.getElementById("potato");
 const tadaSound = new Audio("tada.mp3");
 
 let isFries = false;
+let isLocked = false;
 
 
-// 클릭
+// 감자 누르기
 function clickPotato() {
+
+    // 감튀 상태거나 변신 중이면 클릭 무시
+    if (isLocked) return;
 
     potato.classList.add("pressed");
 
-    if (isFries) return;
 
     const random = Math.random();
 
+
+    // 1% 확률
     if (random < 0.01) {
 
-        potato.src = "fries.png";
+        isLocked = true;
         isFries = true;
 
+
+        // 감튀로 변경
+        potato.src = "fries.png";
+
+
+        // 효과음
         tadaSound.currentTime = 0;
         tadaSound.play();
+
+
+        // 3초 후 다시 감자
+        setTimeout(() => {
+
+            potato.src = "potato.png";
+
+            isFries = false;
+            isLocked = false;
+
+        }, 3000);
     }
 }
 
 
-// 뗄 때
+// 손 뗌
 function releasePotato() {
     potato.classList.remove("pressed");
 }
 
+document.addEventListener("pointerdown", () => {
+    tadaSound.load();
+}, { once: true });
 
 potato.addEventListener("pointerdown", clickPotato);
 potato.addEventListener("pointerup", releasePotato);
